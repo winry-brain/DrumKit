48ff645d2b6c70b27e688c0b92ea6e31ecbf9fcf
Ekaterina Durova <ekaterina.durova@gmail.com>
2022-11-30 13:00:33 +0100

    Reacts on keyboard too now

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-// If () added after function name then function gets executed immediately which we don@t want
-// document.querySelector("button").addEventListener("click", handleClick);
-//
-// function handleClick() {
-//   alert("I got clicked!");
-// }
-
-// Anonimous function
-// document.querySelectorAll("button").addEventListener("click", function () {
-//   alert("I got clicked all the buttons!");
-// });
+//Detecting Button Press
 
 var numberOfButtons = document.querySelectorAll(".drum").length;
 
@@ -18,47 +8,62 @@ for (i = 0; i < numberOfButtons; i++) {
 
     var buttonInnerHTML = this.innerHTML;
 
-    switch (buttonInnerHTML) {
-      case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
+    makeSound(buttonInnerHTML);
 
-      case "a":
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "s":
-        var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "d":
-        var tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      case "j":
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-
-      case "k":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "l":
-        var kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-
-      default:
-        console.log(buttonInnerHTML);
-
-    }
   });
+}
+
+//Detecting keyboard press
+
+document.addEventListener("keypress", function(event) {
+  var buttonPressed = event.key;
+  makeSound(buttonPressed);
+});
+
+// Play sound
+
+function makeSound(key) {
+
+      switch (key) {
+        case "w":
+          var tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+          break;
+
+        case "a":
+          var tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+          break;
+
+        case "s":
+          var tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+          break;
+
+        case "d":
+          var tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+          break;
+
+        case "j":
+          var snare = new Audio("sounds/snare.mp3");
+          snare.play();
+          break;
+
+        case "k":
+          var crash = new Audio("sounds/crash.mp3");
+          crash.play();
+          break;
+
+        case "l":
+          var kick = new Audio("sounds/kick-bass.mp3");
+          kick.play();
+          break;
+
+        default:
+          console.log(buttonInnerHTML);
+
+      }
 }
 //
 // function handleClick() {
